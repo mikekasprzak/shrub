9d262d746025d1a711ade26c4522d3a8e3eadfc2
Mike Kasprzak <mike@sykhronics.com>
2016-09-16 22:56:53 -0400

    added proper cli info

--- src/Core/cli.php
+++ src/Core/cli.php
@@ -1,2 +1,7 @@
 <?php
-// Stuff you should do if you're a CLI application (include me)
+// If you're a CLI script, include this.
+
+if ( php_sapi_name() !== 'cli' ) {
+	error_log("ERROR: CLI Only");
+    exit(-2);
+}
